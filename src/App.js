438cc85a01ef3a2daa9f3800d5e6f35098ed95a2
Quantumfishcake <38376693+Quantumfishcake@users.noreply.github.com>
2018-04-14 23:12:34 +1000

    gOOB

--- src/App.js
+++ src/App.js
@@ -1,33 +1,81 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { range } from 'lodash';
 import './App.css';
 
-const test = [
-  {
-    url: 'http',
-    image: '',
-    name: '',
-  },
-  {
-    url: 'http',
-    image: '',
-    name: '',
-  }
-];
+const players = [{
+  id: '1',
+  firstName: 'Tom',
+  lastName: 'Quant'
+}, {
+  id: '2',
+  firstName: 'Jan',
+  lastName: 'Stedry'
+}, {
+  id: '3',
+  firstName: 'Rob',
+  lastName: 'Bird'
+}, {
+  id: '4',
+  firstName: 'David',
+  lastName: 'Revay'
+}];
+
+const numberOfHoles = 9;
+
+const getInitials = (player) => player.firstName[0] + player.lastName[0];
+
+// for (let i = 0; i < numberOfHoles; i++) {
+//       console.log(i);
+// }
+
+ //{ players.map(player => <div key={player.id}>{player.firstName}</div>) }
+
 
 class App extends Component {
-  render() {
+  constructor() {
+    super();
+    this.state = {
+      game: {
+        '1': [0, 0, 2, 0, 0, 0, 0, 0, 0],
+        '2': [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        '3': [0, 0, 0, 0, 0, 9, 0, 0, 0],
+        '4': [0, 1, 2, 0, 0, 0, 0, 0, 0],
+      }
+    }
+  }
+  handleChange = (playerId, holeId) => (event) => {
+    this.state.game[playerId][holeId] = parseInt(event.target.value);
+    this.setState(this.state);
+    console.log(this.state.game);
+  }
+  render = () => {
+    const holes = range(numberOfHoles);
+    console.log(this.state);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-          <hr />
-          <div>{ test.map(text => <div>some static content: {text}</div>) }</div>
-        </p>
+        <table className="holeTable">
+          <tbody>
+            <tr>
+              <th>Hole</th>
+              { holes.map(hole => <th key={hole}>{ hole + 1}</th>)}
+
+            </tr>
+            { players.map(player => (
+              <tr key={player.id}>
+                <td>{ getInitials(player) }</td>
+                { holes.map(hole => (
+                  <td key={hole}>
+                    <input 
+                      value={ this.state.game[player.id][hole] } 
+                      className="hole" 
+                      onChange={ this.handleChange(player.id, hole) }
+                    />
+                  </td>
+                ))}
+              </tr>
+            )) }
+          </tbody>
+        </table>
       </div>
     );
   }
